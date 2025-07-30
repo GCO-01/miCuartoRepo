37ef6cab75571bad320f34569c5d8c3dccc7131b
GCO-01 <127131769+GCO-01@users.noreply.github.com>
2025-07-29 21:48:05 -0500

    Agregue superheroes Marvel

--- API/app.py
+++ API/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/superheroesMarvel")
+def get_superheroes():
+    rows = ["Ironman", "Capitan America", "Spiderman", "Hulk", "Deadpool", "Fantastic 4"]
+    return rows
